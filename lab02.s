f07f4d96da305a391c6cd362dbe8baaaad98507f
spyrospastromas <cs05011@uoi.gr>
2024-10-22 18:10:49 +0300

    Completed lab02

--- lab02.s
+++ lab02.s
@@ -6,12 +6,21 @@ array: .word 1, 0, 1, 12, 0, 1, 4
 .text
 
     la a0, array
-    li a1, 7    # unsigned
+    li a1, 7    
     li a2, 1
 prog:
 #-----------------------------
-# Write your code here!
-# Do not remove the prog label or write code above it!
+    findLast_backwards_withPointers:
+        beq  a1, zero, ret0
+        slli s0, a1, 2 
+        add  s0, s0, a0
+    loop:
+        addi s0, s0, -4 
+        lw   t1, 0(s0)
+        beq  t1, a2, done
+        bne  s0, a0, loop
+    ret0:
+        add  s0, zero, zero
 #-----------------------------
 done:
     addi a7, zero, 10 
